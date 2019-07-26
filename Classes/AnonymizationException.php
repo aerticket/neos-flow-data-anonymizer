65781409f704df9a9f158250bc6ec70811bb0784
Matthias Klatte <matthias.klatte@gmail.com>
2019-07-26 09:24:15 +0200

    FEATURE: create a method to anonymize given entities
    Implements the method anonymizeEntity which optionally updates the given
    entity in it's repository. For that some internal functionality was
    factored out into reusable methods.

---
+++ Classes/AnonymizationException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aerticket\DataAnonymizer;
+
+/*
+ * This file is part of the Aerticket.DataAnonymizer package.
+ *
+ * (c) Contributors to the package
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Exception;
+
+class AnonymizationException extends Exception
+{
+}
